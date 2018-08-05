52f3f87394309545df5e1046b22e76d6dd14ebff
“michalbac <michal.bac1993@gmail.com>
2018-08-05 14:22:59 +0200

    First attempt to Heroku

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -12,6 +12,7 @@ import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequestMapping("/v1/task")
+@CrossOrigin(origins = "*")
 public class TaskController {
     @Autowired
     private DbService service;
