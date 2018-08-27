2e9bd2460f3eba8c091e9b355891382311b32ebb
Michal.Bac <michal.bac1993@gmail.com>
2018-08-27 13:27:59 +0200

    Updated properties

---
+++ src/main/java/com/crud/tasks/scheduler/EmailScheduler.java
@@ -0,0 +1,20 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailScheduler {
+
+    @Autowired
+    private SimpleEmailService simpleEmailService;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private AdminConfig adminConfig;
+}
